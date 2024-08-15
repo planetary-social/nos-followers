fe17caed867bb1119472700f53ca1cd3b5fc2142
Daniel Cadenas <dcadenas@gmail.com>
2024-08-15 12:32:43 -0300

    Don't break on PublishError

--- src/google_publisher.rs
+++ src/google_publisher.rs
@@ -1,9 +1,9 @@
 use crate::domain::follow_change::FollowChange;
-use anyhow::{Context, Ok, Result};
 use gcloud_sdk::{
     google::pubsub::v1::{publisher_client::PublisherClient, PublishRequest, PubsubMessage},
     *,
 };
+use thiserror::Error;
 use tokio::select;
 use tokio::sync::mpsc::{self, error::SendError};
 use tokio::time::{self, Duration};
@@ -16,12 +16,15 @@ struct GooglePublisherClient {
 }
 
 impl GooglePublisherClient {
-    async fn publish_events(&mut self, follow_changes: Vec<FollowChange>) -> Result<()> {
-        let pubsub_messages: Result<Vec<PubsubMessage>> = follow_changes
+    async fn publish_events(
+        &mut self,
+        follow_changes: Vec<FollowChange>,
+    ) -> Result<(), GooglePublisherError> {
+        let pubsub_messages: Result<Vec<PubsubMessage>, GooglePublisherError> = follow_changes
             .iter()
             .map(|follow_change| {
                 let data = serde_json::to_vec(follow_change)
-                    .context("Failed to serialize event to JSON")?;
+                    .map_err(GooglePublisherError::SerializationError)?;
 
                 Ok(PubsubMessage {
                     data,
@@ -41,7 +44,7 @@ impl GooglePublisherClient {
             .get()
             .publish(request)
             .await
-            .context("Failed to publish follow change")?;
+            .map_err(GooglePublisherError::PublishError)?;
 
         Ok(())
     }
@@ -52,7 +55,9 @@ pub struct GooglePublisher {
 }
 
 impl GooglePublisher {
-    pub async fn create(cancellation_token: CancellationToken) -> Result<Self> {
+    pub async fn create(
+        cancellation_token: CancellationToken,
+    ) -> Result<Self, GooglePublisherError> {
         let google_project_id = "pub-verse-app";
         let google_topic = "follow-changes";
         let google_full_topic = format!("projects/{}/topics/{}", google_project_id, google_topic);
@@ -63,13 +68,14 @@ impl GooglePublisher {
                 "https://pubsub.googleapis.com",
                 Some(google_full_topic.clone()),
             )
-            .await?;
+            .await
+            .map_err(GooglePublisherError::Init)?;
 
         let (publication_sender, mut publication_receiver) = mpsc::channel::<FollowChange>(1);
 
         tokio::spawn(async move {
             let mut buffer = Vec::new();
-            let size_threshold = 1000;
+            let size_threshold = 500;
             let seconds = 5;
 
             let mut client = GooglePublisherClient {
@@ -90,8 +96,27 @@ impl GooglePublisher {
                         if !buffer.is_empty() {
                             info!("Publishing batch of {} follow changes", buffer.len());
                             if let Err(e) = client.publish_events(buffer.split_off(0)).await {
-                                error!("Failed to publish events: {:?}", e);
-                                break;
+                                match &e {
+                                    GooglePublisherError::PublishError(_) => {
+                                        // TODO: We don't break in this error for the moment while investigating the cause:
+                                        // server-1  | Caused by:
+                                        // server-1  |     0: status: Unknown, message: "transport error", details: [], metadata: MetadataMap { headers: {} }
+                                        // server-1  |     1: transport error
+                                        // server-1  |     2: connection error
+                                        // server-1  |     3: peer closed connection without sending TLS close_notify: https://docs.rs/rustls/latest/rustls/manual/_03_howto/index.html#unexpected-eof
+                                        // server-1  |
+                                        // server-1  | Stack backtrace:
+                                        // server-1  |    0: std::backtrace::Backtrace::create
+                                        // server-1  |    1: nos_followers::google_publisher::GooglePublisherClient::publish_events::{{closure}}
+                                        // server-1  |    2: nos_followers::google_publisher::GooglePublisher::create::{{closure}}::{{closure}}k
+
+                                        error!("{}", e);
+                                    }
+                                    _ => {
+                                        error!("Failed to publish events: {}", e);
+                                        break;
+                                    }
+                                }
                             }
                         }
                     }
@@ -133,4 +158,16 @@ impl GooglePublisher {
     ) -> Result<(), SendError<FollowChange>> {
         self.sender.send(follow_change).await
     }
+}
+
+#[derive(Error, Debug)]
+pub enum GooglePublisherError {
+    #[error("Failed to publish events: {0}")]
+    PublishError(#[from] tonic::Status),
+
+    #[error("Failed to serialize event to JSON: {0}")]
+    SerializationError(#[from] serde_json::Error),
+
+    #[error("Failed to initialize Google publisher: {0}")]
+    Init(#[from] gcloud_sdk::error::Error),
 }
